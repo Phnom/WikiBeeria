5c62b1272d76728c2c51b2132bec0a6101d0e012
Simon <soold88@hotmail.se>
2020-11-06 15:46:22 +0100

    gjj
    gjjgj

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 
 // Global url
 const beerUrl = "https://api.punkapi.com/v2/beers/"
+document.querySelector(".form > button").addEventListener("click", function() {
+    document.querySelector(".beer-info").classList.remove("active")
+    document.querySelector(".beer-list").classList.add("active")
+    searchBeer()
+})
 
 async function fetchBeerData(url) {
   const response = await fetch(url)
@@ -12,15 +17,48 @@ async function fetchBeerData(url) {
 async function renderRandomBeer() {
 
   const beerContent = document.querySelector(".beer-random-card-content")
+  const beerImg = document.querySelector(".beer-random-card-img > img")
 
   let beerData = await fetchBeerData(`${beerUrl}random`)
   beerData = beerData[0]
-
+  console.log(beerData)
   beerContent.innerHTML = `
   <h1>${beerData.name}</h1>
-  <p>${beerData.description}</p>
   <button>Read more...</button>
   `
+  beerImg.src = beerData.image_url
+  const randomButton = document.querySelector(".beer-random-card-content > button").addEventListener("click", function() {
+    const randomCard = document.querySelector(".beer-random-card").classList.remove("active")
+    const mainContent = document.querySelector(".main-content").classList.add("active")
 
+
+    renderBeerInfo(beerData)
+  })
 }
-renderRandomBeer()
+async function renderBeerInfo(beerData) {
+    const beerContent = document.querySelector(".beer-info")
+    beerContent.innerHTML = `
+    <div class="beer-info-img">
+    <img src="${beerData.image_url}" alt="">
+    </div>
+    <h1>${beerData.name}</h1>
+    <p>Alcohol Volume: ${beerData.abv}%</p>
+    <p>${beerData.volume.value}${beerData.volume.unit}</p>
+    <p>${beerData.description}</p>
+    `
+}
+async function searchBeer() {
+    const url = "https://api.punkapi.com/v2/beers/"
+    const beerList = document.querySelector(".beer-list")
+    beerList.innerHTML = "<li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li></li>"
+    const beerListItems = document.querySelectorAll(".beer-list > li")
+    let beerData = await fetchBeerData(url)
+    for (let i = 0; i < 10; i++) {
+        beerListItems[i].innerText = beerData[i].name
+    }
+}
+window.onload = (event) => {
+    renderRandomBeer()
+}
+
+
