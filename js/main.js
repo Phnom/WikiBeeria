a13de8bdd19ca0de36dfaea4c0f78b6ef7fd2429
Simon <soold88@hotmail.se>
2020-11-11 13:47:22 +0100

    hej
    hej

--- js/main.js
+++ js/main.js
@@ -190,8 +190,8 @@ async function searchForBeer() {
   const yeastMenu = document.querySelector('#yeast');
   const abvLessMenu = document.querySelector('#abvLesser');
   const abvGreatMenu = document.querySelector('#abvGreater');
-  /*const brewedBeforeMenu = document.querySelector('#brewedBefore');
-  const brewedAfterMenu = document.querySelector('#brewedAfter');*/
+  const brewedBeforeMenu = document.querySelector('#brewedBefore');
+  const brewedAfterMenu = document.querySelector('#brewedAfter');
 
   let query = '?';
   if (searchInput.length > 0) {
@@ -224,18 +224,16 @@ async function searchForBeer() {
   } else if (abvGreatMenu.value.toLowerCase() !== 'abvgreater') {
     query += `abv_gt=${abvGreatMenu.value.toLowerCase()}`;
   }
-  /*
   if (brewedBeforeMenu.value.toLowerCase() !== 'brewedbefore' && query.length > 1) {
-    query += `&brewed_before=${brewedBeforeMenu.value.toLowerCase()}`;
+    query += `&brewed_before=01-${brewedBeforeMenu.value.toLowerCase()}`;
   } else if (brewedBeforeMenu.value.toLowerCase() !== 'brewedbefore') {
-    query += `brewed_before=${brewedBeforeMenu.value.toLowerCase()}`;
+    query += `brewed_before=01-${brewedBeforeMenu.value.toLowerCase()}`;
   }
   if (brewedAfterMenu.value.toLowerCase() !== 'brewedafter' && query.length > 1) {
-    query += `&brewed_after=${brewedAfterMenu.value.toLowerCase()}`;
+    query += `&brewed_after=01-${brewedAfterMenu.value.toLowerCase()}`;
   } else if (brewedAfterMenu.value.toLowerCase() !== 'brewedafter') {
-    query += `brewed_after=${brewedAfterMenu.value.toLowerCase()}`;
+    query += `brewed_after=01-${brewedAfterMenu.value.toLowerCase()}`;
   }
-  */
 
   if (query.length > 1) {
     query += `&per_page=80`;
